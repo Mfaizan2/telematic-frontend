6e6d19729d884f9119a9fc4b116f482d87654140
Muhammad Faizan <faizan@teletrack.au>
2025-03-04 17:58:22 +0500

    More React learning

---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <h1>
+            Welcome to Telematic!
+        </h1> 
+    );
+} 
+
+export default Homepage;
---
+++ src/components/HomepageClassComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class HomepageClassComponent extends Component {
+    render() {
+        return (
+            <h1>
+                Welcome to Telematic(Class Component)!
+            </h1>
+        )
+    }
+}
+
+export default HomepageClassComponent;
